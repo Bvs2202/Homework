1fa651a42dc9971ada215146aaba1952f2caf098
Bvs2202 <135632169+Bvs2202@users.noreply.github.com>
2023-12-15 19:56:32 +0700

    Update triangles.c

--- exercise_2/triangles.c
+++ exercise_2/triangles.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+#define N 5 
+
+int main()
+{
+    int arr[N][N] = {0};
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            if (i + k <= N - 2)
+            {
+                arr[i][k] = 0;
+                printf("%d ", arr[i][k]);
+            }
+            else
+            {
+                arr[i][k] = 1;
+                printf("%d ", arr[i][k]);
+            }
+        }
+        printf("\n");
+    }
+}
